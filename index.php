cabc6ff42ee01acf019124cbda7dd2aa08c2dae3
XAWeb <57809442+XAWeb@users.noreply.github.com>
2020-06-17 22:58:07 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
-require('import.php');
-use XAWEB\SqlImport\Import;
-$filename = 'database.sql';
-$username = '';
-$password = '';
-$database = '';
-$host = 'localhost';
+require('import.php'); //SINIFI DAHİL ETTİK
+use XAWEB\SqlImport\Import; //SINIFI ÇAĞIRDIK
+$filename = 'database.sql'; // Import işlemini gerçekleştirmek istediğiniz veritabanının adını girin.
+$username = ''; // Veritabanı kullanıcı adınız
+$password = ''; // Veritabanı şifreniz
+$database = ''; // Yüklemenin gerçekleşeceği veri tabanı
+$host = 'localhost'; // İşlemin gerçekleştirileceği sunucu
 new Import($filename, $username, $password, $database, $host);
